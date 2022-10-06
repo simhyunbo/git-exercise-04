06b307ca1dd2e349c9b0973a36a7b322e7baa650
simhyunbo <hbo9347@naver.com>
2022-10-06 13:45:01 +0900

    알파벳 개수세기

--- src/MapExercise/AlphabetCnt.java
+++ src/MapExercise/AlphabetCnt.java
@@ -1,7 +1,11 @@
 package MapExercise;
 
+import java.util.HashMap;
+
 public class AlphabetCnt {
+    //2.true false 반환
     public boolean isAlphabet(char c){
+        //(c>='A' && c<='Z') || (c>='a' && c<='z')도 가능하다.
         if((c>=65 && c<=90) || (c>=97 && c<=122)){
             return true;
         }else {
@@ -13,15 +17,27 @@ public class AlphabetCnt {
         AlphabetCnt alphabetCnt = new AlphabetCnt();
         //1.String 한글자씩 출력할 수 있어야 한다.
         //charAt(인덱스)
-        String s1 = "aabbccddeeffakkkrrml";
-        for(int i =0;i<s1.length();i++){
-            System.out.println(s1.charAt(i));
+        String s1 = "aabb///ccdd:eeffak://kkrr.ml".toUpperCase();
+
+        //
+        HashMap<Character,Integer> alphabetMap = new HashMap<>();
+        for(char c = 'A';c<'Z';c++){
+            alphabetMap.put(c,0);
         }
 
-        char ch1 = 'a';
-        char ch2 = '1';
-        System.out.println(alphabetCnt.isAlphabet(ch1));
-        System.out.println(alphabetCnt.isAlphabet(ch2));
+        for(int i =0;i<s1.length();i++){
+            char c = s1.charAt(i);
+            boolean isAlphabet = alphabetCnt.isAlphabet(c);
+            if(isAlphabet){
+                //여기에서만 개수를 센다
+                alphabetMap.put(c,alphabetMap.get(c)+1);
+            }
+        }
+        System.out.println(alphabetMap);
+
+
+
+
 
 
     }
