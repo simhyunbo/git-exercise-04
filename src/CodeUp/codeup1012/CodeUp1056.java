8476dfc06b4227cd42a1ef204ede3138dfb23d43
simhyunbo <hbo9347@naver.com>
2022-10-13 01:08:50 +0900

    CodeUp 1051~1060

---
+++ src/CodeUp/codeup1012/CodeUp1056.java
@@ -0,0 +1,21 @@
+package CodeUp.codeup1012;
+
+import java.util.Scanner;
+
+public class CodeUp1056 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b =sc.nextInt();
+
+        if(a==b){
+            System.out.println(0);
+        }else{
+            System.out.println(1);
+        }
+
+
+
+    }
+}
---
+++ src/CodeUp/codeup1012/CodeUp1059.java
@@ -0,0 +1,15 @@
+package CodeUp.codeup1012;
+
+import java.util.Scanner;
+
+public class CodeUp1059 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        System.out.println(~a);
+
+
+
+    }
+}
