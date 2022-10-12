4bfdd481cad7dd9895003a63a89009831d229865
simhyunbo <hbo9347@naver.com>
2022-10-12 10:01:16 +0900

    CodeUp 4596

---
+++ src/CodeUp/codeup1011/CodeUp4596.java
@@ -0,0 +1,33 @@
+package CodeUp.codeup1011;
+
+import java.util.Scanner;
+
+public class CodeUp4596 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[9][9];
+        int max = arr[0][0];
+        int row = 0;
+        int col = 0;
+
+        for(int i =0;i<9;i++){
+            for (int j =0;j<9;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i =0;i<9;i++) {
+            for (int j = 0; j < 9; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    row = i + 1;
+                    col = j + 1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(row+" "+ col);
+
+    }
+}
