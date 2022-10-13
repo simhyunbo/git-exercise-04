fa3eb65e5e284a6de5c36512804d552ca41d00fc
simhyunbo <hbo9347@naver.com>
2022-10-13 09:45:29 +0900

    BubbleSort

--- src/algori/lion1013.java
+++ src/algori/lion1013.java
@@ -1,19 +1,30 @@
 package algori;
 
 public class lion1013 {
-    public static void main(String[] args) {
-
-        int[] bubbleSort = {7,2,3,9,28,11};
+    public int[] bubble(int[] bubbleSort){
         int result = 0;
-        for(int i = 0 ;i<bubbleSort.length;i++){
-            for(int j = 1;j<bubbleSort.length;j++){
-                if(bubbleSort[i]>bubbleSort[j]){
+        for(int i =1 ;i<bubbleSort.length;i++){
+
+                if(bubbleSort[0]>bubbleSort[i]){
                     result = bubbleSort[i];
-                    bubbleSort[i] = bubbleSort[j];
-                    bubbleSort[j] = result;
+                    bubbleSort[0] = bubbleSort[i];
+                    bubbleSort[i] = result;
                 }
-            }
+
         }
-        System.out.println(bubbleSort[0]);
+        return bubbleSort;
+
+    }
+
+
+    public static void main(String[] args) {
+        lion1013 lion = new lion1013();
+        int[] bubbleSort = {7,2,3,9,28,11};
+
+        int[] result = lion.bubble(bubbleSort);
+        for(int i = 0 ;i<result.length;i++){
+            System.out.print(result[i]+" ");
+        }
+
     }
 }
