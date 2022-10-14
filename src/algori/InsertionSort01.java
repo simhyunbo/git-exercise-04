b52a242beba3876499dc31a33fa7da1e9b8ef122
simhyunbo <hbo9347@naver.com>
2022-10-14 09:48:28 +0900

    InsertionSort Swap

--- src/algori/InsertionSort01.java
+++ src/algori/InsertionSort01.java
@@ -5,8 +5,14 @@ import java.util.Arrays;
 public class InsertionSort01 {
     public int[] sort(int[] arr){
         int i = 1;
+        int swapNum = 0;
+        if(arr[i] < arr[i-1]) {
+            swapNum = arr[i-1];
+            arr[i-1] = arr[i];
+            arr[i] = swapNum;
+        }
 
-        return new int[]{};
+        return arr;
     }
     public static void main(String[] args) {
         //5에서 출발을 합니다.
