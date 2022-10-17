1ec891434047b1df65e25bef355042ccce47d1e0
simhyunbo <hbo9347@naver.com>
2022-10-17 09:10:44 +0900

    RightTriangle

--- src/algori/InsertionSort01.java
+++ src/algori/InsertionSort01.java
@@ -1,26 +1,41 @@
 package algori;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class InsertionSort01 {
     public int[] sort(int[] arr){
-        int i = 1;
-        int swapNum = 0;
-        if(arr[i] < arr[i-1]) {
-            swapNum = arr[i-1];
-            arr[i-1] = arr[i];
-            arr[i] = swapNum;
-        }
 
+        //int swapNum = 0;
+        for(int i = 1 ;i<arr.length;i++){
+            for(int j = i; j>0;j--) {
+                //System.out.printf("i:%d j:%d j-1:%d\n",i,j,j-1);
+                if(arr[j] < arr[j-1]) {
+                    int swapNum = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = swapNum;
+                }
+            }
+        }
         return arr;
     }
     public static void main(String[] args) {
         //5에서 출발을 합니다.
-        int[] arr = {8,5,6,2,4};
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+
         InsertionSort01 is = new InsertionSort01();
         int[] result = is.sort(arr);
 
-        System.out.println(Arrays.toString(result));
+        for(int i = 0 ;i<result.length;i++){
+            System.out.println(result[i]);
+        }
+        //System.out.println(result.length);
+        //System.out.println(Arrays.toString(result));
 
     }
 }
---
+++ src/algori/RightTriangle.java
@@ -0,0 +1,11 @@
+package algori;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
