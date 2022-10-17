a4cbd1425a212c6c2a01e387c1fa6a51cf41abf8
simhyunbo <hbo9347@naver.com>
2022-10-17 10:14:17 +0900

    피라미드형

---
+++ src/algori/pyramid.java
@@ -0,0 +1,22 @@
+package algori;
+
+public class pyramid {
+
+    public void printStar(int n){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i*2+1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pyramid py = new pyramid();
+        py.printStar(5);
+
+    }
+}
