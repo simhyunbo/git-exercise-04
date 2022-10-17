be5757490bab4ab82544ff36f5af9f011e4f3f47
simhyunbo <hbo9347@naver.com>
2022-10-17 09:20:48 +0900

    n개 별찍기

--- src/algori/RightTriangle.java
+++ src/algori/RightTriangle.java
@@ -1,11 +1,22 @@
 package algori;
 
+import java.util.Scanner;
+
 public class RightTriangle {
     public static void main(String[] args) {
-
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        /*
+        * i=0 별이 1개
+        ** i=1 별이 2개
+        *** i=2 별이 3개
+        **** i=3 별이 4개
+         */
+        Scanner sc = new Scanner(System.in);
+        int starNum = sc.nextInt();
+        for(int i =0;i<starNum;i++){
+            for(int j =0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
