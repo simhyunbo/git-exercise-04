1a43d999fafdff6eab266d4062a7fa9679e90ee9
simhyunbo <hbo9347@naver.com>
2022-10-18 09:16:58 +0900

    정사각형

---
+++ src/algori/SquareStar.java
@@ -0,0 +1,17 @@
+package algori;
+
+import java.lang.annotation.Repeatable;
+
+public class SquareStar {
+    public static void main(String[] args) {
+
+        int n = 5;
+        String star = "*";
+        for(int i = 0;i<n;i++){
+
+            System.out.println(star.repeat(n));
+            }
+
+        }
+    }
+
